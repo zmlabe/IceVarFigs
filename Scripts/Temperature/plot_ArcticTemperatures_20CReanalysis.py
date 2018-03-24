a2435e7c4db4ffe616b0a6305f9d36dc70d23eb9
Zachary Labe <zlabe@uci.edu>
2018-03-24 12:49:17 -0700

    20CR update

--- Scripts/Temperature/plot_ArcticTemperatures_20CReanalysis.py
+++ Scripts/Temperature/plot_ArcticTemperatures_20CReanalysis.py
@@ -31,11 +31,11 @@ print('\n' '----Arctic Temperatures (20C Reanalysis Data) - %s----' % titletime)
 
 ## Alott time series
 yearmin = 1900
-yearmax = 2016
+yearmax = 2017
 years = np.arange(yearmin,yearmax+1,1)
 months = [r'Jan',r'Feb',r'Mar',r'Apr',r'May',r'Jun',r'Jul',r'Aug',
           r'Sep',r'Oct',r'Nov',r'Dec']
-datasets = ['20CRv2c','ERA20C','JRA55']
+datasets = ['GISTEMP','Berkeley','ERA20C','JRA55','ERAi']
 
 ### Read in data
 datat = np.empty((len(datasets),len(years)))
@@ -90,7 +90,7 @@ ax.tick_params('both',length=5.5,width=2,which='major')
 plt.plot(np.arange(yearmin,yearmax+2,1),([0]*(len(years)+1)),
          linewidth=2,color='darkgrey',alpha=1,linestyle='--')
 
-color=iter(cmocean.cm.haline(np.linspace(0.14,1,len(datasets))))
+color=iter(cmocean.cm.haline(np.linspace(0.,1,len(datasets))))
 for i in range(len(datasets)):
     c=next(color)
     plt.plot(years,datat[i],linewidth=1.8,color=c,alpha=1,
@@ -98,7 +98,7 @@ for i in range(len(datasets)):
 
 plt.xticks(np.arange(1900,2040,10),np.arange(1900,2040,10))
 plt.yticks(np.arange(-3,4,1),map(str,np.arange(-3,4,1))) 
-plt.xlim([1900,2017])
+plt.xlim([1900,2018])
 plt.ylim([-3,3])
 
 plt.ylabel(r'\textbf{2-m Air Temperature Anomalies ($\bf{^\circ}$C)}',fontsize=13,
@@ -108,12 +108,12 @@ plt.title(r'\textbf{ARCTIC TEMPERATURES',
 plt.text(1983.4,-3.0,r'\textbf{BASELINE: 1981-2010}',
          fontsize=10.1,rotation='horizontal',ha='left',color='darkgrey')
 
-l = plt.legend(shadow=False,fontsize=7,loc='upper right',
-           bbox_to_anchor=(0.73, 1.02),fancybox=True,ncol=3,frameon=False)
+l = plt.legend(shadow=False,fontsize=7,loc='upper center',
+           bbox_to_anchor=(0.5, 1.02),fancybox=True,ncol=5,frameon=False)
 for text in l.get_texts():
     text.set_color('w')   
 
-plt.text(1900,-3.85,r'\textbf{DATA:} NOAA/ESRL Physical Sciences Division [WRIT Tool]',
+plt.text(1900,-3.85,r'\textbf{DATA:} NOAA/ESRL Physical Sciences Division [WRIT Tool; +67$\bf{^\circ}$N]',
          fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
 plt.text(1900,-4.0,r'\textbf{SOURCE:} https://www.esrl.noaa.gov/psd/cgi-bin/data/testdap/timeseries.pl',
          fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
@@ -128,7 +128,7 @@ plt.subplots_adjust(bottom=0.15)
 ### Create subplot of region of averaging (67N+)
 
 ### Add axis for subplot
-a = plt.axes([.35, .567, .29, .24], axisbg='w') 
+a = plt.axes([.35, .59, .29, .24], axisbg='w') 
 
 def setcolor(x, color):
      for m in x:
@@ -150,7 +150,7 @@ meridians = np.arange(-180,180,30)
 m.drawparallels(parallels,labels=[True,True,True,True],
                 linewidth=0.2,color='w')
 par=m.drawmeridians(meridians,labels=[True,True,True,True],
-                    linewidth=0.2,fontsize=4,color='w')
+                    linewidth=0.2,fontsize=3,color='w')
 setcolor(par,'white')
 
 cs = m.contourf(lon2,lat2,lat2,np.arange(67,100,10),latlon=True,
