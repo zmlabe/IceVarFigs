39676eeb8ef002820fdfc1ebf3f49940267797b3
Zachary Labe <zlabe@uci.edu>
2018-08-14 09:24:26 -0700

    Sea Ice Moving

---
+++ Scripts/SeaIce/plot_VolumeExtent_MovingLines.py
@@ -0,0 +1,188 @@
+"""
+Plot change in sea ice extent (NSIDC) and volume (PIOMAS) for annual means
+from 1979 to 2017.
+
+Author    : Zachary M. Labe
+Date      : 14 August 2018
+"""
+
+### Import modules
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math 
+
+### Time
+now = datetime.datetime.now()
+currentmn = str(now.month)
+currentdy = str(now.day)
+currentyr = str(now.year)
+currenttime = currentmn + '_' + currentdy + '_' + currentyr
+currentdoy = now.timetuple().tm_yday
+
+### Add directories
+directorydata = '/home/zlabe/Documents/Projects/IceVarFigs/Data/'
+directoryfigure = '/home/zlabe/Documents/Projects/IceVarFigs/Figures/'
+
+### Load data files for Extent (e) and Antarctica (v)
+filee = 'NSIDC_AnnualSIE_2017.txt'
+filev = 'PIOMAS_AnnualSIV_2017.txt'
+
+### Years through 2017
+years = np.arange(1979,2017+1,1)
+
+### Read file
+eq = np.genfromtxt(directorydata + filee,unpack=True)
+vq = np.genfromtxt(directorydata + filev,unpack=True)                        
+                        
+print('\nCompleted: Read land ice data!')                        
+
+############################################################################
+############################################################################
+############################################################################
+### Create animation
+plt.rc('text',usetex=True)
+plt.rc('font',**{'family':'sans-serif','sans-serif':['Avant Garde']}) 
+plt.rc('savefig',facecolor='black')
+plt.rc('axes',edgecolor='darkgrey')
+plt.rc('xtick',color='darkgrey')
+plt.rc('ytick',color='darkgrey')
+plt.rc('axes',labelcolor='darkgrey')
+plt.rc('axes',facecolor='black')
+
+fig = plt.figure()
+
+### Subplot for Extent
+ax = plt.subplot(121)  
+
+### Adjust axes in time series plots 
+def adjust_spines(ax, spines):
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            spine.set_position(('outward', 5))
+        else:
+            spine.set_color('none')  
+    if 'left' in spines:
+        ax.yaxis.set_ticks_position('left')
+    else:
+        ax.yaxis.set_ticks([])
+
+    if 'bottom' in spines:
+        ax.xaxis.set_ticks_position('bottom')
+    else:
+        ax.xaxis.set_ticks([]) 
+
+plt.text(1979,10.07,r'\textbf{EXTENT}',color='deepskyblue',alpha=0.5,ha='left',
+        fontsize=22,rotation=0,va='center',zorder=1)
+        
+ax.tick_params('both',length=5.5,width=2,which='major')             
+adjust_spines(ax, ['left','bottom'])            
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none') 
+ax.spines['left'].set_linewidth(2)
+ax.spines['bottom'].set_linewidth(2) 
+
+ant, = plt.plot(years,eq,linestyle='-',linewidth=3,
+         color='deepskyblue',zorder=2)
+
+plt.scatter(years[-1],eq[-1],s=30,color='crimson',zorder=9)
+
+xlabels = map(str,np.arange(1979,2019,6))
+plt.xticks(np.arange(1979,2019,6),xlabels,fontsize=9)
+ylabels = map(str,np.arange(10,13,0.5))
+plt.yticks(np.arange(10,13,0.5),ylabels,fontsize=9)
+plt.ylim([10,12.5])
+plt.xlim([1979,2017.8])
+
+plt.text(2019,10.32,r'\textbf{2017}',fontsize=10,color='crimson',ha='left')
+
+plt.text(1979,9.5,r'\textbf{DATA:} NSIDC Sea Ice Index v3.0 (\textbf{ANNUAL}, Satellite)',
+         fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
+plt.text(1979,9.42,r'\textbf{SOURCE:} ftp://sidads.colorado.edu/DATASETS/NOAA/G02135',
+         fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
+plt.text(1979,9.34,r'\textbf{GRAPHIC:} Zachary Labe (@ZLabe)',
+         fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
+         
+plt.text(1968,12.64,r'\textbf{[\textbf{$\times$10$^{6}$ km$^{2}$}]}',color='darkgrey',
+                           fontsize=12,va='center',alpha=1)      
+plt.text(2024,13.1,r'\textbf{ARCTIC SEA ICE}',fontsize=32,color='w',
+         ha='center',va='center',alpha=1)
+
+###########################################################################
+###########################################################################
+###########################################################################
+### Subplot for Volume
+ax = plt.subplot(122)  
+
+### Adjust axes in time series plots 
+def adjust_spines(ax, spines):
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            spine.set_position(('outward', 5))
+        else:
+            spine.set_color('none')  
+    if 'left' in spines:
+        ax.yaxis.set_ticks_position('left')
+    else:
+        ax.yaxis.set_ticks([])
+
+    if 'bottom' in spines:
+        ax.xaxis.set_ticks_position('bottom')
+    else:
+        ax.xaxis.set_ticks([]) 
+        
+ax.tick_params('both',length=5,width=2,which='major')             
+adjust_spines(ax, ['left','bottom'])            
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none') 
+ax.spines['left'].set_linewidth(2)
+ax.spines['bottom'].set_linewidth(2) 
+
+plt.text(1979,12.4,r'\textbf{VOLUME}',color='deepskyblue',alpha=0.5,ha='left',
+        fontsize=22,rotation=0,va='center',zorder=1)
+
+gre, = plt.plot(years,vq,linestyle='-',linewidth=3,
+         color='deepskyblue',zorder=2)
+
+plt.scatter(years[-1],vq[-1],s=30,color='crimson',zorder=9)
+
+xlabels = map(str,np.arange(1979,2019,6))
+plt.xticks(np.arange(1979,2019,6),xlabels,fontsize=9)
+ylabels = map(str,np.arange(12,27,2))
+plt.yticks(np.arange(12,27,2),ylabels,fontsize=9)
+plt.ylim([12,26])
+plt.xlim([1979,2017.8])
+
+plt.text(1979,9.25,r'\textbf{DATA:} PIOMAS v2.1 (\textbf{ANNUAL}, Simulated)',
+         fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
+plt.text(1979,8.75,r'\textbf{SOURCE:} http://psc.apl.washington.edu/zhang/IDAO/data.html',
+         fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
+plt.text(1979,8.29,r'\textbf{REFERENCE:} Zhang and Rothrock [2003]',
+         fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
+
+plt.text(2019,12.6,r'\textbf{2017}',fontsize=10,color='crimson',ha='left')
+plt.text(1966.7,26.8,r'\textbf{[\textbf{$\times$1000 km$^{3}$}]}',color='darkgrey',
+                           fontsize=12,va='center',alpha=1) 
+
+fig.subplots_adjust(wspace=0.4)
+fig.subplots_adjust(top=0.75)
+fig.subplots_adjust(bottom=0.2)
+
+### Create animation using matplotlib
+def update(num,years,eq,vq,ant,gre,bar):
+    ant.set_data(years[:num+1],eq[:num+1])
+    ant.axes.axis([1979,2017.8,10,12.5])
+    gre.set_data(years[:num+1],vq[:num+1])
+    gre.axes.axis([1979,2017.8,12,26])
+    return bar,
+
+ani = animation.FuncAnimation(fig,update,60,fargs=[years,
+                              eq,vq,ant,gre,gre],interval=0.01,blit=True)
+
+### Save figure
+#plt.savefig(directoryfigure + 'SeaIce_moving.png',dpi=300)
+ani.save(directoryfigure + 'SeaIce_moving.gif',dpi=300,writer='imagemagick')
+
+print('\nCompleted: Script done!')
+                     
---
+++ Scripts/Temperature/plot_ArcticAmplification_BEST.py
@@ -0,0 +1,132 @@
+"""
+Scripts plots temperatures from Berkeley Earth contrasting the Arctic and 
+global temperature anomalies - "Arctic Amplification"
+ 
+Notes
+-----
+    Source : https://www.esrl.noaa.gov/psd/cgi-bin/data/testdap/timeseries.pl
+    Author : Zachary Labe
+    Date   : 2 July 2018
+"""
+
+### Import modules
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as c
+import datetime
+import cmocean
+from mpl_toolkits.basemap import Basemap
+
+### Define directories
+directorydata = '/home/zlabe/Documents/Projects/IceVarFigs/Data/'
+directoryfigure = '/home/zlabe/Documents/Projects/IceVarFigs/Figures/'
+
+### Define time           
+now = datetime.datetime.now()
+currentmn = str(now.month)
+currentdy = str(now.day)
+currentyr = str(now.year)
+currenttime = currentmn + '_' + currentdy + '_' + currentyr
+titletime = currentmn + '/' + currentdy + '/' + currentyr
+print('\n' '----Arctic Amplification using BEST - %s----' % titletime) 
+
+## Alott time series
+yearmin = 1900
+yearmax = 2017
+years = np.arange(yearmin,yearmax+1,1)
+months = [r'Jan',r'Feb',r'Mar',r'Apr',r'May',r'Jun',r'Jul',r'Aug',
+          r'Sep',r'Oct',r'Nov',r'Dec']
+datasets = ['Arctic','Global']
+datasetsq = ['Arctic [+67$^\circ$N]','Global']
+
+### Read in data
+datat = np.empty((len(datasets),len(years)))
+for i in range(len(datasets)):
+    datat[i] = np.genfromtxt(directorydata + 'BEST_%s.txt' % (datasets[i]),
+                         delimiter=',',skip_header=1,unpack=True,usecols=[1])
+
+### Look for missing data    
+datat[np.where(datat == -9999.000)] = np.nan
+
+###############################################################################
+###############################################################################
+###############################################################################                 
+#### Plot Figure
+plt.rc('text',usetex=True)
+plt.rc('font',**{'family':'sans-serif','sans-serif':['Avant Garde']}) 
+plt.rc('savefig',facecolor='black')
+plt.rc('axes',edgecolor='darkgrey')
+plt.rc('xtick',color='darkgrey')
+plt.rc('ytick',color='darkgrey')
+plt.rc('axes',labelcolor='darkgrey')
+plt.rc('axes',facecolor='black')
+
+### Adjust axes in time series plots 
+def adjust_spines(ax, spines):
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            spine.set_position(('outward', 5))
+        else:
+            spine.set_color('none')  
+    if 'left' in spines:
+        ax.yaxis.set_ticks_position('left')
+    else:
+        ax.yaxis.set_ticks([])
+
+    if 'bottom' in spines:
+        ax.xaxis.set_ticks_position('bottom')
+    else:
+        ax.xaxis.set_ticks([]) 
+        
+fig = plt.figure()
+ax = plt.subplot(111)
+
+adjust_spines(ax, ['left', 'bottom'])            
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_linewidth(2)
+ax.spines['left'].set_linewidth(2)
+ax.tick_params('both',length=5.5,width=2,which='major')
+
+plt.plot(np.arange(yearmin,yearmax+2,1),([0]*(len(years)+1)),
+         linewidth=0.7,color='darkgrey',alpha=1,linestyle='--',
+         dashes=(1,0.3))
+
+color=iter(cmocean.cm.balance_r(np.linspace(0.15,0.8,len(datasets))))
+for i in range(len(datasets)):
+    c=next(color)
+    plt.plot(years,datat[i],linewidth=3,color=c,alpha=1,
+             label = r'\textbf{%s}' % datasetsq[i])
+    
+    plt.text(np.max(years)+6,datat[i][-1],r'\textbf{2017}',color=c,alpha=1,
+             ha='center',va='center')
+
+plt.xticks(np.arange(1900,2040,10),np.arange(1900,2040,10))
+plt.yticks(np.arange(-10,11,1),map(str,np.arange(-10,11,1))) 
+plt.xlim([1900,2018])
+plt.ylim([-3,4])
+
+plt.ylabel(r'\textbf{Air Temperature Anomalies ($\bf{^\circ}$C)}',fontsize=13,
+                     color='darkgrey')
+plt.title(r'\textbf{ARCTIC AMPLIFICATION',
+                    color='darkgrey',fontsize=25)
+plt.text(1983.4,-3.0,r'\textbf{BASELINE: 1981-2010}',
+         fontsize=10.1,rotation='horizontal',ha='left',color='darkgrey')
+
+l = plt.legend(shadow=False,fontsize=7,loc='upper center',
+           bbox_to_anchor=(0.5, 1.02),fancybox=True,ncol=2,frameon=False)
+for text in l.get_texts():
+    text.set_color('w')   
+    
+plt.subplots_adjust(bottom=0.15)
+
+plt.text(1900,-4.05,r'\textbf{DATA:} Berkeley Earth Data using NOAA/ESRL Physical Sciences Division [WRIT Tool]',
+         fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
+plt.text(1900,-4.23,r'\textbf{SOURCE:} https://www.esrl.noaa.gov/psd/cgi-bin/data/testdap/timeseries.pl',
+         fontsize=5,rotation='horizontal',ha='left',color='darkgrey',alpha=1)
+plt.text(2017.6,-4.05,r'\textbf{GRAPHIC:} Zachary Labe (@ZLabe)',
+         fontsize=5,rotation='horizontal',ha='right',color='darkgrey',alpha=1)   
+
+### Save figure           
+plt.savefig(directoryfigure+'ArcticAmplification_BEST_%s.png' % yearmax,dpi=300)
+
